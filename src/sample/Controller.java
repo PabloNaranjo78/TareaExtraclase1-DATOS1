8c0b296a867ffa5cd4410fd60df5ba10bda02098
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2020-09-19 15:00:40 -0600

    Agregar campos de texto y botones

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
-public class Controller {
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import org.w3c.dom.Text;
+
+import java.awt.*;
+import javafx.scene.control.TextField;
+
+import javax.management.StringValueExp;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public TextField textCampo;
+    @FXML
+    public TextField textCampo1;
+    public TextArea mensajes;
+
+    public void newMessage(ActionEvent actionEvent) {
+
+        String op = String.valueOf(this.textCampo.getText());
+
+        this.mensajes.setText(mensajes.getText()+"\n"+"Tu: "+ op);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
 }
